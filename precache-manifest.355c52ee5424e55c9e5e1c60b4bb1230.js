3fe8fd74e89364afd316c1b1266b2db273f6b982
Alex Young <alex.young@hive.hr>
2019-08-23 11:43:27 +0100

    Updates

---
+++ precache-manifest.355c52ee5424e55c9e5e1c60b4bb1230.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a51284cea739565629bf5ef75d20b887",
+    "url": "/emotion-demo/index.html"
+  },
+  {
+    "revision": "5ad74aa31365e3494ab3",
+    "url": "/emotion-demo/static/js/2.4b1a2b2a.chunk.js"
+  },
+  {
+    "revision": "fd416f500a583ea60ec9",
+    "url": "/emotion-demo/static/js/main.bd517428.chunk.js"
+  },
+  {
+    "revision": "c5173c186aa9581110c8",
+    "url": "/emotion-demo/static/js/runtime~main.6d4bec1f.js"
+  }
+]);
