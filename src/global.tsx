f0b539efa6bb40be2d8778373401ced89b99ae9d
Alex Young <alex.young@hive.hr>
2019-08-23 11:55:31 +0100

    amazeballs

--- src/global.tsx
+++ src/global.tsx
@@ -18,6 +18,12 @@ export const GlobalStyles = () => (
                         ? "green"
                         : "initial"};
                     padding: 10px;
+                    background-image: ${theme.name === Themes.fancy
+                        ? 'url("http://amazeballsarancini.com.au/wp-content/uploads/2018/07/amazeballs-logo@2x.png")'
+                        : "none"};
+                    background-repeat: repeat-y;
+                    background-position-x: center;
+                    background-position-y: top;
                 }
             `;
         }}
